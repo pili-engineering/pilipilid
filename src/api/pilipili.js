31463f81cff3001a47df0886e134b4271c02a49a
harebu <bugu1986@126.com>
2016-08-03 11:32:32 +0800

    rest API

--- src/api/pilipili.js
+++ src/api/pilipili.js
@@ -1,5 +1,9 @@
 import resource from 'resource-router-middleware';
-import facets from '../models/pilipili';
+import pilipilis from '../models/pilipili';
+import Pili from 'pili';
+
+const credentials = new Pili.Credentials("MqF35-H32j1PH8igh-am7aEkduP511g-5-F7j47Z", "BF9QHMKIUQp_Oh4Xk8SwyhmwJ0CO-9n-RJzDgZQr");
+const hub = new Pili.Hub(credentials, "NIU7PS");
 
 export default ({ config, db }) => resource({
 
@@ -10,41 +14,73 @@ export default ({ config, db }) => resource({
 	 *  Errors terminate the request, success sets `req[id] = data`.
 	 */
 	load(req, id, callback) {
-		let facet = facets.find( facet => facet.id===id ),
-			err = facet ? null : 'Not found';
-		callback(err, facet);
+		let pilipili = pilipilis.find( pilipili => pilipili.id===id ),
+			err = pilipili ? null : 'Not found';
+		callback(err, pilipili);
 	},
 
 	/** GET / - List all entities */
 	index({ params }, res) {
-		res.json(facets);
+		res.json(pilipilis);
 	},
 
 	/** POST / - Create a new entity */
 	create({ body }, res) {
-		body.id = facets.length.toString(36);
-		facets.push(body);
-		res.json(body);
+    let title = body.title | null;
+    var options = {
+      title          : body.title,    // optional, auto-generated as default
+      publishKey     : null,    // optional, auto-generated as default
+      publishSecurity : "static" // optional, can be "dynamic" or "static", "dynamic" as default
+    };
+
+    hub.createStream(options, function(err, stream) {
+      if (!err) {
+        pilipilis.push(stream);
+        res.json(stream);
+      } else {
+        console.log(err + 'error code: ' + err.errorCode + 'http code: ' + err.httpCode);
+        res.json({error:err})
+      }
+    });
+
 	},
 
 	/** GET /:id - Return a given entity */
-	read({ facet }, res) {
-		res.json(facet);
+	read({ pilipili }, res) {
+    console.log(pilipili);
+		res.json(pilipili);
 	},
 
 	/** PUT /:id - Update a given entity */
-	update({ facet, body }, res) {
-		for (let key in body) {
-			if (key!=='id') {
-				facet[key] = body[key];
-			}
-		}
+	update({ pilipili, body }, res) {
+		//for (let key in body) {
+		//	if (key!=='id') {
+		//		pilipili[key] = body[key];
+		//	}
+		//}
 		res.sendStatus(204);
 	},
 
 	/** DELETE /:id - Delete a given entity */
-	delete({ facet }, res) {
-		facets.splice(facets.indexOf(facet), 1);
-		res.sendStatus(204);
+	delete({ pilipili }, res) {
+    //pilipilis.splice(pilipilis.indexOf(pilipili), 1);
+    //res.sendStatus(204);
+    hub.getStream(pilipili.id, function(err, stream) {
+      if (!err) {
+        console.log(stream);
+        stream.delete(function(err, data) {
+          if (!err) {
+            console.log(data);
+            pilipilis.splice(pilipilis.indexOf(pilipili), 1);
+            res.sendStatus(204);
+          } else {
+            console.log(err + 'error code: ' + err.errorCode + 'http code: ' + err.httpCode);
+            res.json({error:err})
+          }
+
+        });
+      }
+    });
+
 	}
 });
