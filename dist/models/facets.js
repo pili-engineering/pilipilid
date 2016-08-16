495b33a0e3ea99aedc9ecd54ba983d3b4ce3b05c
harebu <bugu1986@126.com>
2016-08-16 11:13:05 +0800

    rm dist dir

--- dist/models/facets.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-// our example model is just an Array
-var facets = [];
-exports.default = facets;
-//# sourceMappingURL=facets.js.map
